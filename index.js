e156ff1959d4be3775e73b3578e749715504c198
Victoria Meng <nights1ststar@protonmail.com>
2018-08-16 00:52:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 function driversWithRevenueOver(drivers, revenue) {
   return drivers.filter(driver => driver.revenue > revenue);
 };
-
 function driverNamesWithRevenueOver(drivers, revenue) {
   const filtered_drivers = driversWithRevenueOver(drivers, revenue);
   return filtered_drivers.map(driver => driver.name);
 };
+function exactMatch() {
+
+}
